5b705507b3427aeb569d35a93a1057fabbe20e9a
Hari K Gourabathuni <109535284+hkrishg@users.noreply.github.com>
2023-02-05 20:02:10 +0000

    duplicates scenario added

--- Main.java
+++ Main.java
@@ -2,6 +2,18 @@ import java.util.*;
 
 class Main {
 
+//   Problem Statement #
+// Given a set with distinct elements, find all of its distinct subsets.
+
+// Example 1:
+
+// Input: [1, 3]
+// Output: [], [1], [3], [1,3]
+// Example 2:
+
+// Input: [1, 5, 3]
+// Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3]
+
   public List<List<Integer>> subset(int[] nums) {
     List<List<Integer>> subsets = new ArrayList<>();
     subsets.add(new ArrayList<>());
@@ -20,11 +32,44 @@ class Main {
     return subsets;
   }
 
+//   Problem Statement #
+// Given a set of numbers that might contain duplicates, find all of its distinct subsets.
+
+// Example 1:
+
+// Input: [1, 3, 3]
+// Output: [], [1], [3], [1,3], [3,3], [1,3,3]
+  
+    public List<List<Integer>> subsetDupl(int[] nums) {
+      Arrays.sort(nums);
+    List<List<Integer>> subsets = new ArrayList<>();
+    subsets.add(new ArrayList<>());
+    int startIndex = 0, endIndex = 0;
+      for (int i = 0; i < nums.length; i++){
+        startIndex = 0;
+        //if current and the prev elements are same, create new subsets only from the subsets added in the prev steps
+        if(i > 0 && nums[i] == nums [i-1])
+          startIndex = endIndex +1;
+        endIndex = subsets.size() - 1;
+        for(int j = startIndex; j <= endIndex; j++ ){
+                  // create a new subset from existing subset and insert the current element
+        List<Integer> set = new ArrayList<>(subsets.get(j)); 
+        set.add(nums[i]); //
+        subsets.add(set);//
+        }
+      }
+    return subsets;
+  }
+
   public static void main(String[] args) {
     Main main = new Main();
 
-    List<List<Integer>> result = main.subset(new int[] { 1, 3 });
+    List<List<Integer>> result = main.subset(new int[] { 1, 5, 3 });
 
     System.out.println(result);
+    
+        List<List<Integer>> result1 = main.subsetDupl(new int[] { 1, 3, 3 });
+
+    System.out.println(result1);
   }
 }
